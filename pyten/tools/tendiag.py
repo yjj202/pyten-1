87d0f2d8889b1dfdafff2d02c9a837ba909b8617
CraigAlanFriedman <47229093+CraigAlanFriedman@users.noreply.github.com>
2021-06-10 01:35:14 -0400

    added case for n=1

--- pyten/tools/tendiag.py
+++ pyten/tools/tendiag.py
@@ -16,6 +16,7 @@ def tendiag(v, sz):
     x = np.zeros(sz)
     subs = np.matlib.repmat(np.array(range(n)).reshape(n, 1), 1, len(sz))
 
+    #add case for n=1
     if(n>1):
       for i in range(n):
           x[subs[i][0], subs[i][1], subs[i][2]] = v[i]
