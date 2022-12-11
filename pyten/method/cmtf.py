9922bb741a2d3c2fdc42b7cb11716f2a9c77f4c7
yjj202 <86861251+yjj202@users.noreply.github.com>
2022-12-10 21:18:49 -0500

    Update cmtf.py

--- pyten/method/cmtf.py
+++ pyten/method/cmtf.py
@@ -50,7 +50,9 @@ def cmtf(x, y=None, c_m=None, r=2, omega=None, tol=1e-4, maxiter=100, init='rand
     dimorder = range(N)  # 'dimorder' - Order to loop through dimensions {0:(ndims(A)-1)}
 
     # Define convergence tolerance & maximum iteration
-    fitchangetol = 1e-4
+    #fitchangetol = 1e-4
+    fitchangetol = tol # changed by yjj
+    print('THIS IS THE CORRECT CODE------')
     maxiters = maxiter
 
     # Recover or just decomposition
